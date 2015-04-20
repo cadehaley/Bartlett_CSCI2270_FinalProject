058cdc5acce7e84b20df2ca61ce4d045dc80cb84
Jack Bartlett <jacbart@gmail.com>
2015-04-20 17:27:06 -0600

    Updated Header and cpp

--- AddressBook.cpp
+++ AddressBook.cpp
@@ -5,5 +5,19 @@ AddressBook::AddressBook(){
 }
 
 AddressBook::~AddressBook(){
-	
+
 }
+
+void AddressBook::addContact(){}
+
+void AddressBook::deleteContact(){}
+
+void AddressBook::printContacts(){}
+
+void AddressBook::findContact(){}
+
+void AddressBook::editContact(){}
+
+void AddressBook::groupCreate(){}
+
+void AddressBook::addContactToGroup(){}
